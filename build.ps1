ec506c1a0477257efbe9fa3a8a0c3c16b145c22e
Ondřej Hošek <ondrej.hosek@tuwien.ac.at>
2025-09-04 17:44:38 +0200

    build.ps1: allow opt-out of programming

--- build.ps1
+++ build.ps1
@@ -1,3 +1,8 @@
+[CmdletBinding()]
+Param(
+    [switch]$NoProgram
+)
+
 & cargo build --release
 if ($LASTEXITCODE -ne 0) {
     Throw "Rust build failed"
@@ -8,6 +13,8 @@ If ($LASTEXITCODE -ne 0) {
     Throw "ELF-to-bin conversion failed"
 }
 
-& 'C:\Program Files\OpenOCD\bin\openocd.exe' `
-    -c "source mikroe-click4-stm32f74.cfg" `
-    -c "program temp-pair-enocean.bin 0x08000000 verify reset exit"
+If (-not $NoProgram) {
+    & 'C:\Program Files\OpenOCD\bin\openocd.exe' `
+        -c "source mikroe-click4-stm32f74.cfg" `
+        -c "program temp-pair-enocean.bin 0x08000000 verify reset exit"
+}
